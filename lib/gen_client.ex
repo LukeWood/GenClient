a8c0d5559e193d5ff8458440a59b6fff4d99ec29
Luke Wood <lukewoodcs@gmail.com>
2017-12-28 12:04:03 -0800

    genclient now creates the client code

--- lib/gen_client.ex
+++ lib/gen_client.ex
@@ -1,8 +1,6 @@
 defmodule GenClient do
   defmacro __using__(for: module) do
 
-    base_module = (elem(module,2) |> Enum.at(0))
-
     module_expanded = Macro.expand(module, __ENV__)
     functions = module_expanded.__info__(:functions)
 
@@ -10,15 +8,14 @@ defmodule GenClient do
       &generate_client_definitions/1
     )
 
-    result = quote do
-      defmodule unquote(base_module).Client do
+     quote do
+      defmodule Client do
         unquote_splicing(client_definitions)
       end
+      defmodule Server do
+        use GenServer
+      end
     end
-
-    IO.puts(Macro.to_string result)
-
-    result
   end
 
   #TODO(lukewood) use the real function names from the impl
@@ -28,14 +25,13 @@ defmodule GenClient do
 
     client_args = generate_client_args(arity)
     call_args = call_args(client_args, function_name)
+    pid_arg = Enum.at(client_args, 0)
 
-    ast = quote do
+    quote do
       def unquote(function_name)(unquote_splicing(client_args)) do
-        GenServer.call(pid, {unquote_splicing(call_args)})
+        GenServer.call(unquote(pid_arg), {unquote_splicing(call_args)})
       end
     end
-
-    ast
   end
 
   defp call_args(client_args, function_name) do
@@ -73,8 +69,6 @@ defmodule Placeholder do
 
       args = generate_call_args(function_name, arity)
       params = generate_call_params(args)
-
-      IO.inspect(args)
 
       quote do
         def handle_call(unquote(args), _from, state) do
--- test/gen_client_test.exs
+++ test/gen_client_test.exs
@@ -1,7 +1,25 @@
 defmodule GenClientTest do
   use ExUnit.Case
 
+  test "GenClient generates TestModule.Client methods" do
+    increment_created = Enum.any?(TestModule.Client.__info__(:functions), fn
+      {:increment, 2} -> true
+      _ -> false
+    end)
+    assert increment_created
+
+    increment_by_created = Enum.any?(TestModule.Client.__info__(:functions), fn
+      {:increment_by, 3} -> true
+      _ -> false
+    end)
+    assert increment_by_created
+  end
+
   test "GenClient in testmodule" do
-    TestModule.handle_call({:increment}, self(), 1) |> IO.inspect
+    TestModule.Server.handle_call({:increment}, self(), 1) |> IO.inspect
+  end
+
+  test "TestModule.Client test" do
+    TestModule.Client.increment()
   end
 end

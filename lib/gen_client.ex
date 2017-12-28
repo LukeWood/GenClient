562df8e446768d7cd5424e7ff49b812c5a0329ad
Luke Wood <lukewoodcs@gmail.com>
2017-12-28 10:49:15 -0800

    initial skeleton

--- lib/gen_client.ex
+++ lib/gen_client.ex
@@ -3,6 +3,30 @@ defmodule GenClient do
     module_expanded = Macro.expand(module, __ENV__)
 
     functions = module_expanded.__info__(:functions)
-    IO.inspect functions
+    client_definitions = Enum.map(functions, &generate_client_definitions/1)
+    server_definitions = Enum.map(functions, &generate_server_definitions/1)
+
+    quote do
+      unquote(client_definitions)
+      unquote(server_definitions)
+    end
+  end
+
+  defp generate_client_definitions(function) do
+    quote do
+      def meow() do
+        IO.inspect("MEOW")
+      end
+    end
+  end
+
+  defp generate_server_definitions(_function) do
+    args = {:meow}
+    function = fn x->x+1 end
+    quote do
+      def handle_call(unquote(args), _from, state) do
+        unquote(function(state))
+      end
+    end
   end
 end

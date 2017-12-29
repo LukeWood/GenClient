6f71ba0fdc90698004de247bcfd88384cd9f6c9b
Luke Wood <lukewoodcs@gmail.com>
2017-12-28 18:28:13 -0800

    reorg

--- lib/gen_client.ex
+++ lib/gen_client.ex
@@ -2,38 +2,26 @@ defmodule GenClient do
 
   @moduledoc """
     The GenClient module is to be used with the use macro.
-    
+
     GenClient handles the interface between your state management logic and
     the GenServer library.
   """
 
   defmacro __using__(for: module, calls: calls, casts: casts) do
-
+    module_expanded = Macro.expand(module, __ENV__)
     calls = Macro.expand(calls, __ENV__)
     casts = Macro.expand(casts, __ENV__)
 
-    module_expanded = Macro.expand(module, __ENV__)
-    Code.ensure_compiled?(module_expanded)
     calls = get_arities(module_expanded, calls)
     casts = get_arities(module_expanded, casts)
 
-    client_calls = Enum.map(calls,
-      &GenClient.Client.call_definition/1
-    )
-    client_casts = Enum.map(casts,
-      &GenClient.Client.cast_definition/1
-    )
-
-    server_calls = Enum.map(calls,
-      &GenClient.Server.call_definition(module, &1)
-    )
-    server_casts = Enum.map(casts,
-      &GenClient.Server.cast_definition(module, &1)
-    )
+    client_calls = Enum.map(calls, &GenClient.Client.call_definition/1)
+    client_casts = Enum.map(casts, &GenClient.Client.cast_definition/1)
+    server_calls = Enum.map(calls, &GenClient.Server.call_definition(module, &1))
+    server_casts = Enum.map(casts, &GenClient.Server.cast_definition(module, &1))
 
     client = GenClient.Client.create(client_calls, client_casts)
     server = GenClient.Server.create(server_calls, server_casts)
-
     quote do
       unquote(client)
       unquote(server)
